1a6528461869d36d165e16616adf9dd8c1a931c4
Ryan Archibald <ryanarchibald9219@gmail.com>
2017-11-20 18:46:00 +0000

    Array and Class
    Adding Array and Class projects

---
+++ ClassExample/ClassExample/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassExample
+{
+    public class Person
+    {
+        public String FirstName;
+        public string LastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, my name is: " + FirstName + " " + LastName);
+        }
+    }
+}
---
+++ ClassExample/ClassExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ClassExample.Math;
+
+namespace ClassExample
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ryan = new Person();
+            ryan.FirstName = "Ryan";
+            ryan.LastName = "Archibald";
+            ryan.Introduce();
+
+            var calc1 = new Calculator();
+            var result = calc1.Add(4, 7);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
