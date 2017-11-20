4c818e365b9f80edb837cc8ea255312beea1537c
Ryan Archibald <ryanarchibald9219@gmail.com>
2017-11-20 09:47:48 +0000

    Initial commit

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Type Conversion Example
+            /*byte b = 1;
+            int i = b;
+            Console.WriteLine(i);*/
+
+            //Explicit Type Conversion
+            //int i = 1;
+            //byte b = (byte)i; //(byte) this is a CAST, no data loss will happen as byte can store up to 255
+            // Console.WriteLine(b);
+
+            //Non-Compatible Type Conversion
+            //try
+            //{
+            //    string number = "1234";
+            //    int no = int.Parse(number);
+            //    Console.WriteLine(no);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("The number could not be converted.");
+            //}
+
+            try
+            {
+                string str = "true";
+                bool boolean = Convert.ToBoolean(str);
+                Console.WriteLine(boolean);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Can not convert string to boolean");
+            }
+        }
+    }
+}
---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue); //format string
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
